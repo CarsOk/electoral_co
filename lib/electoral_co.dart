935854ecbe3e83a15e5592a533e94d00b496d621
Jhon Fredy Santander <jsantander1219@gmail.com>
2024-11-22 16:46:52 -0500

    button outline

--- lib/electoral_co.dart
+++ lib/electoral_co.dart
@@ -1,7 +1,18 @@
 library electoral_co;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class ButtonOutlineWidget extends StatelessWidget {
+  const ButtonOutlineWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
